0a0ff99f13886b6ad944079dfa8d5322b74af5a8
ocirak <omercirak25@gmail.com>
2019-09-15 16:23:41 +0300

    Initial Commit

---
+++ smackApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smackApp
+//
+//  Created by Ömer Çırak on 9/15/19.
+//  Copyright © 2019 Ömer Çırak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
